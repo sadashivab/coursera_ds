d7229865df41c35b3b3a9dd9a36b155001189f7b
sadashivab <sadashivab@yahoo.com>
2015-04-25 19:23:15 +0100

    removed test fork files

--- 1024/README.md
+++
@@ -1,2 +0,0 @@
-This is a clone of 2048 made using javascript.
-Work in progress.
--- 1024/js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-		
-		var game = new Game();
-		game.initializeBoard();
-		game.setRandomBoard();
-		game.setEventListeners();
-		game.redrawScreenFromArray();
-		
-});
